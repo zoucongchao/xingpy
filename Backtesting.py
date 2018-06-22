bbca144b7d730881e1f970124c21a9f3cb9c346f
zoucongchao <798422668@qq.com>
2018-06-22 14:40:10 +0800

    a1

--- Backtesting.py
+++ Backtesting.py
@@ -10,6 +10,9 @@ import pymongo
 from vnpy.trader.vtGlobal import globalSetting
 
 from vnpy.trader.vtObject import VtTickData, VtBarData
+from vnpy.trader.app.ctaStrategy.ctaBase import MINUTE_DB_NAME
+
+from datetime import datetime,timedelta
 
 ########################################################################
 class BacktestingEngine(object):
@@ -28,6 +31,7 @@ class BacktestingEngine(object):
         
         self.dbClient = None        # 数据库客户端
         self.dbCursor = None        # 数据库指针
+        self.collection = None
         
         self.initData = []          # 初始化用的数据
         self.dbName = ''            # 回测数据库名
@@ -45,16 +49,51 @@ class BacktestingEngine(object):
         self.mode = mode
         
     #----------------------------------------------------------------------
-    
-    
+    def setStartDate(self, startDate='20100416', initDays=10):
+        """设置回测的启动日期"""
+        self.startDate = startDate
+        self.initDays = initDays
+        
+        self.dataStartDate = datetime.strptime(startDate, '%Y%m%d')
+        #print self.dataStartDate
+        
+        initTimeDelta = timedelta(initDays)
+        #print initTimeDelta
+        
+        self.strategyStartDate = self.dataStartDate + initTimeDelta
+        
+        #print self.strategyStartDate
+        
+    #----------------------------------------------------------------------
+    def setDatabase(self, dbName, symbol):
+        """设置历史数据所用的数据库"""
+        self.dbName = dbName
+        self.symbol = symbol
+        
+    #----------------------------------------------------------------------
+    def setEndDate(self, endDate=''):
+        """设置回测的结束日期"""
+        self.endDate = endDate
+        
+        if endDate:
+            self.dataEndDate = datetime.strptime(endDate, '%Y%m%d')
+            #print self.dataEndDate
+            
+            # 若不修改时间则会导致不包含dataEndDate当天数据
+            self.dataEndDate = self.dataEndDate.replace(hour=23, minute=59)
+            #print self.dataEndDate
+        
+                   
     #----------------------------------------------------------------------    
     def load_History_Data(self):
+        self.setDatabase(MINUTE_DB_NAME, 'IF0000')
         "加载历史数据"
         self.dbClient = pymongo.MongoClient(globalSetting['mongoHost'], globalSetting['mongoPort'])
-        colledtion = self.dbClient[self.dbName][self.symbol]
+        self.collection = self.dbClient[self.dbName][self.symbol]
         
         self.output(u'开始载入数据')
         
+        """
         # 首先根据回测模式，确认要使用的数据类
         if self.mode == self.BAR_MODE:
             dataClass = VtBarData
@@ -62,7 +101,34 @@ class BacktestingEngine(object):
         else:
             dataClass = VtTickData
             func = self.newTick
+        """
             
-            
+        # 载入初始化需要用的数据
+        flt = {'datetime':{'$gte':self.dataStartDate,
+                           '$lt':self.strategyStartDate}}        
+        self.initCursor = self.collection.find(flt).sort('datetime')
         
+        
+        # 载入回测数据
+        if not self.dataEndDate:
+            flt = {'datetime':{'$gte':self.strategyStartDate}}
+        else:
+            flt = {'datetime':{'$gte':self.strategyStartDate,
+                               '$lte':self.dataEndDate}}
+        self.dbCursor = self.collection.find(flt).sort('datetime')
+       
+        
+                               
+                       
+        
+        
+        
+aa =  BacktestingEngine()
+
+bb = aa.setStartDate() 
+
+ee = aa.setEndDate(endDate='20160508') 
+
+dd = aa.load_History_Data()
+    
         
